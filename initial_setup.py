48adb1bbbf67653ec62f102c31d19e47e9c3f95d
Jordan Lund <jordan.l.lund@gmail.com>
2012-10-15 19:26:39 +0100

    fixed backup_remove second arguement miss

--- initial_setup.py
+++ initial_setup.py
@@ -23,28 +23,28 @@ BIN_LINKS = {'vcprompt': 'vcprompt'}
 def start():
     # thus far lets just make the appropriate links and backup any existing
     # destinations
-    make_links(links=HOME_LINKS, root_link_path=ABS_HOME_PATH)
-    make_links(links=BIN_LINKS, root_link_path=ABS_BIN_PATH)
+    make_links(links=HOME_LINKS, base_link_path=ABS_HOME_PATH)
+    make_links(links=BIN_LINKS, base_link_path=ABS_BIN_PATH)
 
 
-def make_links(links, root_link_path):
+def make_links(links, base_link_path):
     for target, link in links.iteritems():
         abs_target_path = os.path.join(ABS_DOTFILES_PATH, target)
-        abs_link_path = os.path.join(root_link_path, link)
+        abs_link_path = os.path.join(base_link_path, link)
         base_notification = '%s to %s' % (abs_target_path, abs_link_path)
 
         try:
             if os.path.exists(abs_link_path):
-                backup_and_remove(link)
+                backup_and_remove(link, base_link_path)
             print 'symlinking ' + base_notification
             os.symlink(abs_target_path, abs_link_path)
         except:
             print 'could not link ' + base_notification
 
 
-def backup_and_remove(dest, root_link_path):
+def backup_and_remove(dest, base_link_path):
     """backs up the file that already exists then removes it"""
-    abs_src_path = os.path.join(root_link_path, dest)
+    abs_src_path = os.path.join(base_link_path, dest)
     abs_dest_path = os.path.join(ABS_BACKUP_DIR, dest)
     base_notification = '%s to %s' % (abs_src_path, abs_dest_path)
 
