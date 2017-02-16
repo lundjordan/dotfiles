31c6aad1de6986f998adea67910c64b54f95b5a3
Jordan Lund <jlund@mozilla.com>
2017-02-15 16:49:34 -0800

    merges bash and zsh into zsh configs, cp back oh-my-zsh configs to dotfiles repo

---
+++ zsh/aliases.zsh
@@ -0,0 +1,169 @@
+#######################################
+# shortcut aliases and functions
+#######################################
+
+
+#############
+#permanent aliases
+#############
+
+# taskwarrior alias's
+alias tstw='task due:today project:warrior'
+alias tsaw='task project:warrior'
+alias taaw='task add priority:M project:warrior'
+alias tatw='task add priority:M due:today project:warrior'
+
+alias tstm='task due:today project:mozharness'
+alias tsam='task project:mozharness'
+alias taam='task add priority:M project:mozharness'
+alias tatm='task add priority:M due:today project:mozharness'
+
+alias tstp='task due:today project:promotion'
+alias tsap='task project:promotion'
+alias tatp='task add priority:H due:today project:promotion'
+alias taap='task add priority:H project:promotion'
+
+alias tstd='task due:today project:releaseduty'
+alias tsad='task project:releaseduty'
+alias tatd='task add priority:H due:today project:releaseduty'
+alias taad='task add priority:H project:releaseduty'
+
+alias tstr='task due:today project:reviews'
+alias tsar='task project:reviews'
+alias tatr='task add priority:H due:today project:reviews'
+alias taar='task add priority:H project:reviews'
+
+alias tstc='task due:today project:tcmigration'
+alias tsac='task project:tcmigration'
+alias tatc='task add priority:H due:today project:tcmigration'
+alias taac='task add priority:H project:tcmigration'
+
+alias tsts='task due:today project:shipit'
+alias tsas='task project:shipit'
+alias tats='task add priority:H due:today project:shipit'
+alias taas='task add priority:H project:shipit'
+
+alias tstx='task due:eow project:macos-setup'
+alias tsax='task project:macos-setup'
+alias tatx='task add priority:H due:today project:macos-setup'
+alias taax='task add priority:H project:macos-setup'
+
+# alias tstl='task due:today project:loantool'
+# alias tsal='task project:loantool'
+# alias tatl='task add priority:H due:today project:loantool'
+# alias taal='task add priority:H project:loantool'
+# 
+# alias tstb='task due:today project:mh-builds'
+# alias tsab='task project:mh-builds'
+# alias tatb='task add priority:M due:today project:mh-builds'
+# alias taab='task add priority:M project:mh-builds'
+# 
+# alias tstw='task due:today project:wasabi'
+# alias tsaw='task project:wasabi'
+# alias tatw='task add priority:H due:today project:wasabi'
+# alias taaw='task add priority:M project:wasabi'
+# 
+# alias tstd='task due:today project:buildduty'
+# alias tsad='task project:buildduty'
+# alias tatd='task add priority:M due:today project:buildduty'
+# alias taad='task add priority:M project:buildduty'
+# 
+# alias tstv='task due:today project:vs2013'
+# alias tsav='task project:vs2013'
+# alias tatv='task add priority:M due:today project:vs2013'
+# alias taav='task add priority:M project:vs2013'
+# 
+# alias tstx='task due:today project:blackbox'
+# alias tsax='task project:blackbox'
+# alias tatx='task add priority:M due:today project:blackbox'
+# alias taax='task add priority:M project:blackbox'
+# 
+# alias tstk='task due:today project:apk'
+# alias tsak='task project:apk'
+# alias tatk='task add priority:M due:today project:apk'
+# alias taak='task add priority:M project:apk'
+# 
+# alias tsth='task due:today project:hg-repos'
+# alias tsah='task project:hg-repos'
+# alias tath='task add priority:M due:today project:hg-repos'
+# alias taah='task add priority:M project:hg-repos'
+
+alias tata='task add due:today'
+alias tt='task due:today'
+
+alias tsar='task project:reviews'
+alias tatr='task add priority:H due:today project:reviews'
+
+
+####### Configs
+# source zsh
+alias sz='. ~/.zshrc'
+# alias's to dirs
+alias cdot='cd ~/devel/personal/dotfiles'
+# alias to aliases
+alias via='vim ~/devel/personal/dotfiles/bash/aliases'
+# zsh alias shortcut
+alias viaz='vim ~/devel/personal/dotfiles/zsh/aliases'
+# my zsh config
+alias vic='vim ~/devel/personal/dotfiles/zsh/config'
+# my zsh config
+alias vie='vim ~/devel/personal/dotfiles/bash/env'
+# alias to vimrc
+alias vimrc='vim ~/.vimrc'
+# alias to vimrc
+alias vimnotes='vim ~/devel/personal/dotfiles/notes/vim_cheat_sheet.txt'
+########
+
+# tmux
+# alias tmux="TERM=screen-256color-bce tmux -2"
+alias tmum="~/.tmux/tmux_mozilla"
+
+
+# Mozilla functions
+
+function peop ()
+{
+        filename=$(basename $1)
+        scp $1 people.mozilla.org:public_html/
+        echo "http://people.mozilla.org/~jlund/${filename}"
+}
+
+ggrel ()
+{
+    grep --color=always --exclude-dir coverage -rnC 6 "$1" . | less -R
+}
+
+# general
+alias -g L="| less" # Write L after a command to page through the output.
+alias -g G="| grep"
+alias -g TL='| tail -20'
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+
+# MOZILLA
+alias -g PEOP="people.mozilla.org:/home/jlund/public_html"
+
+# file paths
+# alias -g DOT='~/devel/personal/dotfiles/'
+# alias -g DEV='~/devel'
+# alias -g MOZ='~/devel/mozilla'
+# alias -g LEA='~/devel/learn/'
+# alias -g LEAA='~/devel/learn/android/'
+# alias -g LEAC='~/devel/learn/C'
+# alias -g LEAN='~/devel/learn/node'
+# alias -g LEAB='~/devel/learn/backbone'
+# alias -g LEAP='~/devel/learn/patterns/'
+# alias -g GOF='~/devel/learn/final_project/goferIT'
+# alias -g DOW='~/Downloads/'
+# alias -g DOC='~/Documents/'
+
+# gofer alias
+alias -g GCOF='coffee -w -c -o public/js backbone-src/**/*.coffee'
+
+
+alias todo=task
+alias dled='ls -t ~/Downloads/**/* | head -n1'
+alias ubunotes='vim ~/devel/personal/dotfiles/notes/ubuntu_fresh_install.md'
+
+# oh-my-zsh overrides
---
+++ zsh/env.zsh
@@ -0,0 +1,76 @@
+export EDITOR=/usr/bin/vim
+
+# #lynx homepage
+# export WWW_HOME=http://www.google.com
+# 
+# export PATH=~/bin:$PATH
+# export PATH=$PATH:~/.android-sdk-linux/tools:~/.android-sdk-linux/platform-tools
+# export PATH=/usr/local/share/python:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
+
+# AWS
+# export JAVA_HOME="/usr/lib/jvm/jdk1.7.0_25/"
+# export AWS_AUTO_SCALING_HOME=~/devel/AWS/AutoScaling-1.0.61.2
+# export PATH="$PATH:$AWS_AUTO_SCALING_HOME/bin:$JAVA_HOME/bin"
+# from aws re:invent
+# export AWS_CREDENTIAL_FILE=~/devel/AWS/jlund-credentials
+# export AWS_AUTO_SCALING_URL=https://autoscaling.eu-west-1.amazonaws.com
+
+
+# vim and gnome-terminal have support for 256 colours in fedora 8 at least
+# Note debian/ubuntu users need to install the ncurses-term package for this
+# Note this should be set in ~/.profile for Fedora startup scripts to
+# setup LS_COLORS correctly.
+# export TERM=xterm-256color
+
+# Use vim to browse man pages. One can use Ctrl-[ and Ctrl-t
+# to browse and return from referenced man pages. ZZ or q to quit.
+# Note initially within vim, one can goto the man page for the
+# word under the cursor by using [section_number]K.
+# Note we use bash explicitly here to support process substitution
+# which in turn suppresses the "Vim: Reading from stdin..." warning.
+export MANPAGER='bash -c "vim -MRn -c \"set ft=man nomod nolist nospell nonu\" \
+-c \"nm q :qa!<CR>\" -c \"nm <end> G\" -c \"nm <home> gg\"</dev/tty <(col -b)"'
+
+# GROFF_NO_SGR is required with man-db which uses grotty to
+# output SGR codes by default when attached to a terminal.
+# We want vim to highlight the raw text, so disable that feature.
+# Also see MANPAGER in my .vimrc for extra handling to support
+# ctrl-[ with man-db
+export GROFF_NO_SGR=1
+# Another option is to use $VIMRUNTIME/macros/less.sh
+# though I find that less effective
+
+# DIRS ###########
+
+# Mozilla Environment
+export BUD=~/devel/mozilla/repos/
+export BUDST=~/devel/mozilla/repos/buildbotcustom/
+export BUDCF=~/devel/mozilla/repos/buildbot-configs/
+export PAT=~/devel/mozilla/patches/
+export LOG=~/devel/mozilla/logs/
+export NOT=~/devel/mozilla/notes/
+export MOZ=~/devel/mozilla/
+export DEV_MASTER=~/devel/mozilla/dev_master/build-master/
+
+# other dirs
+export DOT=~/devel/personal/dotfiles/
+export DEV=~/devel
+export LEA=~/devel/learn/
+export DOW=~/Downloads/
+export DOC=~/Documents/
+
+
+export WORKON_HOME=$HOME/.virtualenvs
+VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+source /usr/local/bin/virtualenvwrapper.sh
+export PYTHONPATH=~/devel/mozilla/dev_master/:~/devel/mozilla/dev_master/tools/lib/python/
+# bpythonrc path
+export PYTHONSTARTUP=~/.pythonrc
+unset VIRTUAL_ENV
+
+##################
+# GITHUB 
+# homebrew token for looking up casks. only scope allowed is for
+# public repo GET so it's safe to be in plain text
+HOMEBREW_GITHUB_API_TOKEN="fb4d17bdcd2e90db5fbabc19100f4e77d18f9d2d"
